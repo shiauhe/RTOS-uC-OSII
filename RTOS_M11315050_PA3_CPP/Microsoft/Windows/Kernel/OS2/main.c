82a999e57d30c0230c183b9e1598ec6a5feb544c
unknown <shiauhe0107@gmail.com>
2025-05-28 18:45:15 +0800

    PA3

---
+++ RTOS_M11315050_PA3_CPP/Microsoft/Windows/Kernel/OS2/main.c
@@ -0,0 +1,268 @@
+/*
+*********************************************************************************************************
+*                                            EXAMPLE CODE
+*
+*               This file is provided as an example on how to use Micrium products.
+*
+*               Please feel free to use any application code labeled as 'EXAMPLE CODE' in
+*               your application products.  Example code may be used as is, in whole or in
+*               part, or may be used as a reference only. This file can be modified as
+*               required to meet the end-product requirements.
+*
+*               Please help us continue to provide the Embedded community with the finest
+*               software available.  Your honesty is greatly appreciated.
+*
+*               You can find our product's user manual, API reference, release notes and
+*               more information at https://doc.micrium.com.
+*               You can contact us at www.micrium.com.
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*
+*                                              uC/OS-II
+*                                            EXAMPLE CODE
+*
+* Filename : main.c
+*********************************************************************************************************
+*/
+
+/*
+*********************************************************************************************************
+*                                            INCLUDE FILES
+*********************************************************************************************************
+*/
+
+#include  <cpu.h>
+#include  <lib_mem.h>
+#include  <os.h>
+
+#include  "app_cfg.h"
+
+
+/*
+*********************************************************************************************************
+*                                            LOCAL DEFINES
+*********************************************************************************************************
+*/
+
+
+/*
+*********************************************************************************************************
+*                                       LOCAL GLOBAL VARIABLES
+*********************************************************************************************************
+*/
+#define TASK_STACKSIZE 2048
+static  OS_STK  StartupTaskStk[APP_CFG_STARTUP_TASK_STK_SIZE];
+
+
+/*
+*********************************************************************************************************
+*                                         FUNCTION PROTOTYPES
+*********************************************************************************************************
+*/
+
+static  void  StartupTask (void  *p_arg);
+static void task(void* p_arg);
+
+/*
+*********************************************************************************************************
+*                                                main()
+*
+* Description : This is the standard entry point for C code.  It is assumed that your code will call
+*               main() once you have performed all necessary initialization.
+*
+* Arguments   : none
+*
+* Returns     : none
+*
+* Notes       : none
+*********************************************************************************************************
+*/
+
+int  main (void)
+{
+#if OS_TASK_NAME_EN > 0u
+    CPU_INT08U  os_err;
+#endif
+
+
+    CPU_IntInit();
+
+    Mem_Init();                                                 /* Initialize Memory Managment Module                   */
+    CPU_IntDis();                                               /* Disable all Interrupts                               */
+    CPU_Init();                                                 /* Initialize the uC/CPU services                       */
+    
+    OSTimeSet(0);
+    OSInit();                                                   /* Initialize uC/OS-II                                  */
+
+    
+    OutFileInit();
+    InputFile();
+
+    OS_ERR err;
+
+
+
+    Task_STK = malloc(TASK_NUMBER * sizeof(int*));
+
+    int n;
+    for (n = 0; n < TASK_NUMBER; n++) {
+        Task_STK[n] = malloc(TASK_STACKSIZE * sizeof(int));
+    }
+
+    //OSTaskCreateExt( StartupTask,                               /* Create the startup task                              */
+    //                 0,
+    //                &StartupTaskStk[APP_CFG_STARTUP_TASK_STK_SIZE - 1u],
+    //                 APP_CFG_STARTUP_TASK_PRIO,
+    //                 APP_CFG_STARTUP_TASK_PRIO,
+    //                &StartupTaskStk[0u],
+    //                 APP_CFG_STARTUP_TASK_STK_SIZE,
+    //                 0u,
+    //                (OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR));
+
+    for (int i = 0; i < TASK_NUMBER; i++) {
+        printf("%d start\n", i);
+
+        OSTaskCreateExt(task,
+        &TaskParameter[i],
+        &Task_STK[i][TASK_STACKSIZE - 1],
+        TaskParameter[i].TaskPriority,
+        TaskParameter[i].TaskID,
+        &Task_STK[i][0],
+        TASK_STACKSIZE,
+        &TaskParameter[i],
+        (OS_TASK_OPT_STK_CHK | OS_TASK_OPT_STK_CLR));
+
+        printf("%d and %d\n", i, TASK_NUMBER);
+
+    }
+    R1 = OSMutexCreate(R1_PRIO, &err);
+    printf("ERR: %d\n", err);
+    R2 = OSMutexCreate(R2_PRIO, &err);
+    printf("ERR: %d\n", err);
+    printf("Tick\tEvent\t\tCurrentTask ID\tNextTask ID\tResponseTime\tBlockingTime\tPreemption\n");
+
+#if OS_TASK_NAME_EN > 0u
+    OSTaskNameSet(         APP_CFG_STARTUP_TASK_PRIO,
+                  (INT8U *)"Startup Task",
+                           &os_err);
+#endif
+    OSStart();                                                  /* Start multitasking (i.e. give control to uC/OS-II)   */
+
+    while (DEF_ON) {                                            /* Should Never Get Here.                               */
+        ;
+    }
+}
+
+
+/*
+*********************************************************************************************************
+*                                            STARTUP TASK
+*
+* Description : This is an example of a startup task.  As mentioned in the book's text, you MUST
+*               initialize the ticker only once multitasking has started.
+*
+* Arguments   : p_arg   is the argument passed to 'StartupTask()' by 'OSTaskCreate()'.
+*
+* Returns     : none
+*
+* Notes       : 1) The first line of code is used to prevent a compiler warning because 'p_arg' is not
+*                  used.  The compiler should not generate any code for this statement.
+*********************************************************************************************************
+*/
+
+static  void  StartupTask (void *p_arg)
+{
+   (void)p_arg;
+
+    OS_TRACE_INIT();                                            /* Initialize the uC/OS-II Trace recorder               */
+
+#if OS_CFG_STAT_TASK_EN > 0u
+    OSStatTaskCPUUsageInit(&err);                               /* Compute CPU capacity with no task running            */
+#endif
+
+#ifdef CPU_CFG_INT_DIS_MEAS_EN
+    CPU_IntDisMeasMaxCurReset();
+#endif
+    
+    APP_TRACE_DBG(("uCOS-III is Running...\n\r"));
+
+    while (DEF_TRUE) {                                          /* Task body, always written as an infinite loop.       */
+        OSTimeDlyHMSM(0u, 0u, 1u, 0u);
+		APP_TRACE_DBG(("Time: %d\n\r", OSTimeGet()));
+    }
+}
+
+void task(void* p_arg) {
+    task_para_set* task_data;
+    task_data = p_arg;
+	int count = 0,old_prio = 63;
+    OS_ERR err;
+    while (1) {
+        if(DEBUG)
+        printf("Tick: %d, start from task%d\n", OSTime, task_data->TaskID);
+        old_prio = OSTCBCur->OSTCBPrio;
+		count = OSTCBCur->TaskExecutionTime - OSTCBCur->TASKWorkLoad;
+        if (count == task_data->lockR1 && task_data->unlockR1 != 0) {
+            printf("%2d\tTRY LockResource\ttask(%2d)(%2d) %d %d\tR1\n", OSTime, OSTCBCur->OSTCBId, OSTCBCur->JobNum,count,R_USED);
+            OSMutexPend(R1, 0, &err);
+            if (R_USED > R1_PRIO)
+                R_USED = R1_PRIO;
+            printf("%2d\tLockResource\ttask(%2d)(%2d)\tR1\t%d to %d\n", OSTime, OSTCBCur->OSTCBId, OSTCBCur->JobNum, OSTCBCur->Ceil,R1_PRIO);
+            fprintf(Output_fp, "%2d\tLockResource\ttask(%2d)(%2d)\tR1\t%d to %d\n", OSTime, OSTCBCur->OSTCBId, OSTCBCur->JobNum, OSTCBCur->Ceil, R1_PRIO);
+			OSTCBCur->Ceil = R1_PRIO; 
+
+        }
+        if (count == task_data->lockR2 && task_data->unlockR2 != 0) {
+            printf("%2d\tTRY LockResource\ttask(%2d)(%2d) %d %d\tR2\n", OSTime, OSTCBCur->OSTCBId, OSTCBCur->JobNum,count,R_USED);
+            OSMutexPend(R2, 0, &err);
+            if (R_USED > R2_PRIO)
+                R_USED = R2_PRIO;
+            printf("%2d\tLockResource\ttask(%2d)(%2d)\tR2\t%d to %d\n", OSTime, OSTCBCur->OSTCBId, OSTCBCur->JobNum, OSTCBCur->Ceil, R2_PRIO);
+            fprintf(Output_fp, "%2d\tLockResource\ttask(%2d)(%2d)\tR2\t%d to %d\n", OSTime, OSTCBCur->OSTCBId, OSTCBCur->JobNum, OSTCBCur->Ceil, R2_PRIO);
+			OSTCBCur->Ceil = R2_PRIO;
+        }
+        if (count == task_data->unlockR1 && task_data->unlockR1 != 0) {
+            printf("%2d\tTRY UnlockResource\ttask(%2d)(%2d)\tR1\n", OSTime, OSTCBCur->OSTCBId, OSTCBCur->JobNum);
+            if ((OS_TCB*)R2->OSEventPtr != (void*)0)
+                R_USED = R2_PRIO;
+            else
+                R_USED = 63;
+            if((OS_TCB*)R2->OSEventPtr == OSTCBCur)
+				OSTCBCur->Ceil = R2_PRIO;
+			else
+				OSTCBCur->Ceil = OSTCBCur->OSTCBPrio;
+            printf("%2d\tUnlockResource\ttask(%2d)(%2d)\tR1\t%d to %d\n", OSTime, OSTCBCur->OSTCBId, OSTCBCur->JobNum,R1_PRIO, OSTCBCur->Ceil);
+            fprintf(Output_fp, "%2d\tUnlockResource\ttask(%2d)(%2d)\tR1\t%d to %d\n", OSTime, OSTCBCur->OSTCBId, OSTCBCur->JobNum, R1_PRIO, OSTCBCur->Ceil);
+            err = OSMutexPost(R1);
+        }
+        if (count == task_data->unlockR2 && task_data->unlockR2 != 0) {
+            printf("%2d\tTRY UnlockResource\ttask(%2d)(%2d)\tR2\n", OSTime, OSTCBCur->OSTCBId, OSTCBCur->JobNum);
+            if ((OS_TCB*)R1->OSEventPtr != (void*)0)
+                R_USED = R1_PRIO;
+            else
+                R_USED = 63;
+            if ((OS_TCB*)R1->OSEventPtr == OSTCBCur)
+                OSTCBCur->Ceil = R1_PRIO;
+            else
+                OSTCBCur->Ceil = OSTCBCur->OSTCBPrio;
+            printf("%2d\tUnlockResource\ttask(%2d)(%2d)\tR2\t%d to %d\n", OSTime, OSTCBCur->OSTCBId, OSTCBCur->JobNum, R2_PRIO, OSTCBCur->Ceil);
+            fprintf(Output_fp, "%2d\tUnlockResource\ttask(%2d)(%2d)\tR2\t%d to %d\n", OSTime, OSTCBCur->OSTCBId, OSTCBCur->JobNum, R2_PRIO, OSTCBCur->Ceil);
+            err = OSMutexPost(R2);
+
+
+        }
+        if(DEBUG)
+        printf("Tick: %d, end from task%d\n", OSTime, task_data->TaskID);
+        count++;
+        OSTimeDly(task_data->TaskPeriodic);
+        //OSTimeDly(0);
+    }
+}
+
+
+
+
+
